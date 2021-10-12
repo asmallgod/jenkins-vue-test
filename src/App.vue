4a5bcf42afa6a442e0dee7266eedd13f375976e8
asmallgod <licheng1996121@gmail.com>
2021-10-12 13:08:53 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Jenkins Test App" />
+  <HelloWorld msg="Welcome to Jenkins Test App hahahha" />
 </template>
 
 <script>
